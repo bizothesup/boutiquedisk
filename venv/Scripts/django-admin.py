bd882aaad126e8ecff563fc215862abf320e23b7
babacar sarr <mbayesarr.baba@gmail.com>
2019-09-20 12:06:42 +0000

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\babacar sarr\PycharmProjects\boutiquedisk\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
